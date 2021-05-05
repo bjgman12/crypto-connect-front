54a7ed5c53ec8352875fe13dc6628806564fcd31
bjgman12 <bjgman38@gmail.com>
2021-05-04 18:58:03 -0700

    strict search bar

---
+++ components/searchBar.js
@@ -0,0 +1,30 @@
+import {useState} from 'react'
+
+export default function BarForm({onSearch}){
+    const initialValues = {
+        query : ''
+    }
+
+    const [values,setValues] = useState(initialValues);
+    
+    function submitHandler(e){
+        e.preventDefault();
+        onSearch(values);
+        setValues(initialValues)
+    }
+
+    function inputChangeHandler(event){
+        let { name , value } = event.target;
+
+        setValues({... values, [name]: value });
+    }
+
+    return (
+        <>
+        <form onSubmit={submitHandler}>
+            <input name='query' type='query' id='query' value={values.query} onChange={inputChangeHandler} placeholder='Search Crypto Here'/>
+            <button type='submit'>search</button>
+        </form>
+        </>
+    );
+}
--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,53 @@ import Coin from "../components/Coins";
 import CoinListed from "../components/listed";
 import React,{ useEffect,useState,useContext } from 'react'
 import coinGecko from '../services/coinGecko'
+import BarForm from '../components/searchBar'
 
 
 
-export default function Home({filteredCoins}) {
+export default function Home() {
+    
+    const [coinData,setCoinData] = useState([])
+    const [displayData,setDisplayData] = useState([])
+    const [coinParam,setCoinParam] = useState('')
+    const [coinPage,setCoinPage] = useState(1)
+
+    const getServerSideProps = async() => {
+    
+        const res = await coinGecko.get('/coins/markets/', {
+                params:{
+                    vs_currency: 'usd',
+                    ids: coinParam
+                },
+            });
+        
+        let filteredCoins = await res.data;
+        setCoinData(filteredCoins);
+
+        }
+    
+    getServerSideProps()
+        
+        function searchHandler(values) {
+            setCoinParam(values.query)
+            
+            
+        }
+        
+        function pageHandler(e){
+            console.log(e.target)
+        }
 
     return (
         <>
         <h1> Header here</h1>
         <p>Mission statement here</p>
-        <p> search bar here</p>
-        <CoinListed filteredCoins={filteredCoins}/>
+        <BarForm onSearch={searchHandler}/>
+        <CoinListed filteredCoins={coinData}/>
+        <form onSubmit={pageHandler}>
+            <button> Next </button>
+            <button> Prev</button>
+        </form>
         <p> news here </p>
         <p> footer here </p>
 
@@ -20,24 +56,5 @@ export default function Home({filteredCoins}) {
     )
 }
 
-export const getServerSideProps = async() => {
-    const res = await coinGecko.get('/coins/markets/', {
-        params:{
-            vs_currency: 'usd'
-        },
-    });
 
-
-let filteredCoins = await res.data;
-
-console.log(filteredCoins)
-
-
-
-return {
-    props:{
-        filteredCoins
-    }
-};
-}
 
