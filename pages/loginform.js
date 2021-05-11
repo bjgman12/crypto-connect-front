8524308cc30d276d65d801104f990dc04c610e0a
bjgman12 <bjgman38@gmail.com>
2021-05-10 19:39:53 -0700

    cookie fest

--- pages/loginform.js
+++ pages/loginform.js
@@ -3,13 +3,27 @@ import Footer from '../components/footer'
 import Header from '../components/header'
 import BannerLog from '../components/logFormComps/logBanner'
 import LogForm from '../components/logFormComps/login'
+import { getToken, login } from '../services/login'
 
 export default function LogSign(){
+
+    const [token,setToken] = useState();
+    const [email,setEmail] = useState('');
+    const [username,setUsername] = useState('user@email.com')
+
+    async function loginHanlder(values){
+        console.log('values at loginForm',values)
+        const fetchedToken = await getToken(values) 
+        setToken(fetchedToken)
+        console.log(token)
+        setUsername(values.email)
+    }
+
 
     return (
         <div className='h-screen bg-white  w-screen'>
         <Header/>
-        <LogForm/>
+        <LogForm onSubmit={loginHanlder}/>
         <Footer/>
         </div>
     )
--- services/login.js
+++ services/login.js
@@ -1,13 +1,13 @@
 import axios from 'axios'
 
-export const login=' https://stage-jlab-crypto.herokuapp.com/api/auth/login'
+export const login='https://stage-jlab-crypto.herokuapp.com/api/auth/login/'
 
 export async function getToken(values) {
-    
-
+    console.log(values)
     const response = await axios.post(login,values);
 
-    return response.data.access;
+    return response;
 
 
 }
+
