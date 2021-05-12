8e6e04fa79e9867d13f433a746bac7454dd9a588
Logan Jones <loganemilyjones@mac.com>
2021-05-12 13:18:06 -0700

    generic table support coins/history

--- components/wallet/coins.js
+++ components/wallet/coins.js
@@ -1,19 +1,25 @@
-import React, {useState, useEffect} from 'react'
 import { currentCoins } from '/services/transactions'
+import Table from './tableHelper'
+
 
 export default function Coins({ transactions }){
-    
-    const [coins, setCoins] = useState('placeholder')
 
-    useEffect(() => {
-        let response = currentCoins(transactions)
-        console.log(response)
-    });
+    const actualCoins = currentCoins(transactions)
+    const columns = [
+        {
+          Header: 'Coin',
+          accessor: 'coin'
+        }, 
+        {
+          Header: 'Units',
+          accessor: 'units'
+        }, 
+      ]
     
     return(
         <div className='w-11/12 pt-2 mx-auto mt-8 rounded-lg bg-gradient-to-b from-gray-300 via-gray-200 to-white'>
             <h2 className='pl-2 text-3xl text-purple-800'> Owned Currencies</h2>
-            <p>{ coins }</p>
+            <Table data={actualCoins} columns={columns}></Table>
         </div>
     )
 }
