7f7852811d8dbc86b8b4e6a4fb951e7d60f2f69f
bjgman12 <bjgman38@gmail.com>
2021-05-11 20:16:56 -0700

    user_id available in cookie

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ export default function Home(props) {
     const [coinPage,setCoinPage] = useState(0)
 
     let resData = ''
-    console.log(token)
+    console.log(Cookies.get('user_id'))
     
     useEffect(() => {
         const request = axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
