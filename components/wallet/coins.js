f3b90347a29e89af24d77000ca29d26b72deaf05
Logan Jones <loganemilyjones@mac.com>
2021-05-12 16:45:56 -0700

    table formatting, add sorting

--- components/wallet/coins.js
+++ components/wallet/coins.js
@@ -17,8 +17,8 @@ export default function Coins({ transactions }){
       ]
     
     return(
-        <div className='w-11/12 p-2 mx-auto mt-8 rounded-lg bg-gradient-to-b from-gray-100 via-gray-50 to-white'>
-            <h2 className='text-3xl text-purple-800'> Owned Currencies</h2>
+        <div className='w-11/12 p-2 mx-auto mt-8 font-light rounded-lg bg-gradient-to-b from-gray-100 via-gray-50 to-white '>
+            <h2 className='text-3xl text-purple-800'>Coin Portfolio</h2>
             <Table data={actualCoins} columns={columns}></Table>
         </div>
     )
--- components/wallet/tableHelper.js
+++ components/wallet/tableHelper.js
@@ -1,17 +1,24 @@
-import {useTable} from 'react-table'
+import {useTable, useSortBy} from 'react-table'
+import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/solid'
 
 export default function Table({ columns, data }) {
-    const {getTableProps, getTableBodyProps, headerGroups, rows, prepareRow} = useTable({columns, data})
+    const {getTableProps, getTableBodyProps, headerGroups, rows, prepareRow} = useTable({columns, data}, useSortBy)
     
     return (
-        <table {...getTableProps()}>
+        <table {...getTableProps()} className="w-full my-2 font-light text-left table-auto ">
+        
           <thead>
             {headerGroups.map(headerGroup => (
               <tr {...headerGroup.getHeaderGroupProps()}>
                 {headerGroup
                   .headers
                   .map(column => (
-                    <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+                    <th className ="font-normal border-b-2" {...column.getHeaderProps(column.getSortByToggleProps())}>{column.render('Header')}
+                      <span>{column.isSorted ? column.isSortedDesc ? 
+                        <ChevronDownIcon className="inline h-4 pl-1 text-gray-500"/> : 
+                        <ChevronUpIcon className="inline h-4 pl-1 text-gray-500"/>: ''}
+                      </span>
+                    </th>
                   ))}
               </tr>
             ))}
