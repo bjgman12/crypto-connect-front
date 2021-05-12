11701bf47e688f64bf28be09d52e1b720a57c581
Logan Jones <loganemilyjones@mac.com>
2021-05-12 16:44:15 -0700

    switch to lobster font

--- components/mission.js
+++ components/mission.js
@@ -2,10 +2,10 @@ export default function Mission() {
 
     return(
         <>
-        <h1 className='pt-2  text-5xl text-center text-purple-900 bg-transparent font-megrim'>Crypto-Connect</h1>
-        <div className='mt-3 text-center text-purple-900 rounded-md'>
-        <p className=''>All the Knowldege, None of the risk.</p>
-        </div>
+            <h1 className='pt-2 text-5xl text-center text-purple-900 bg-transparent font-lobster'>Crypto-Connect</h1>
+            <div className='mt-4 text-center text-purple-900 rounded-md'>
+            <p className='font-light'>All the knowledge, none of the risk.</p>
+            </div>
         </>
     )
 }
