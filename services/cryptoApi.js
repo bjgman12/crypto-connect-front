5ce74196a3e87a4e90078bddae2833a47ec0091f
Logan Jones <loganemilyjones@mac.com>
2021-05-14 10:56:43 -0700

    move watchlistdel into cryptoapi

--- services/cryptoApi.js
+++ services/cryptoApi.js
@@ -71,6 +71,15 @@ export async function postUserCre(values) {
 }
 
 
+export async function delWatchList(token,id) {
+    const tokenAPI = `watchlist/${id}`
+    const config = makeConfig(token)
+    const response = await axios.delete(apiBase + tokenAPI, config)
+    return response.data
+}
+
+
+
 function makeConfig(token) {
     return {
         headers: {
